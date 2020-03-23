bea6090dad1ce5d28f46f7c46b9996d5f2000495
Kim_MiHyun <k696366k@gmail.com>
2020-03-23 15:44:03 +0900

    Hotfix브랜치로 파일 추가함. 200323-3시 43분에 추가됨.

---
+++ dev_book/src/hotfix/HotfixTest.java
@@ -0,0 +1,10 @@
+package hotfix;
+
+public class HotfixTest {
+
+	public static void main(String[] args) {
+		//프로젝트 명 ---> 오른쪽 ---> remote ---> switch ---> new branch ----> hotfix
+		System.out.println("hotfix 브랜치에서 추가한 소스임.");
+	}
+
+}
