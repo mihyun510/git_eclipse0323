90ce630c89444303c51deecef2303e0b3d16c691
Kim_MiHyun <k696366k@gmail.com>
2020-03-23 16:42:34 +0900

    hotfix브랜치에서 저장점을 세개 더 가지기 위한 소스 추가

---
+++ dev_book/src/hotfix/HotfixTest3.java
@@ -0,0 +1,10 @@
+package hotfix;
+
+public class HotfixTest3 {
+	public static void main(String[] args) {
+		//3번째로 추가한 소스임 2번째거 머지가 안됨
+		//hotfix브랜치에서 저장점을 세개 더 가지기 위한 소스 추가
+		System.out.println("hotfix 브랜치에서 추가한 소스임.");
+	}
+
+}
